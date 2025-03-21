5b6de78d20ab6a53a38127edab284dc61e466877
ne3lob <neslob.wr@gmail.com>
2025-03-22 00:08:10 +0100

    add project to try with gitatribute

---
+++ Build/Build.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cab069bdc7a68919f624846079b8c16b58b07815d1d3412912fb2ab65c70aa3c
+size 456403
---
+++ Build/Build.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e3660bd38d470f1ba0c303e1104fcb45cf4c62dc53ae7db5d8375a3c44b54351
+size 19001
