70a61559cc77dbccfc621f640dff75a409627db3
ne3lob <neslob.wr@gmail.com>
2025-03-25 19:33:02 +0100

    new interaction

--- Build/Build.loader.js
+++ Build/Build.loader.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38c0d4c59fa3893eab4ccbf3658be54e43332c8ea07513dddc558ab53e45d757
+oid sha256:926ed8b1ffb58a921b2bb898e53759fa9f6e82b5c4fe889db186ec61bb462abf
 size 25588
